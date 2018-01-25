0deabb7b624c2b516c5f2ec27ad1941f835f6401
xSevithx <sevithstarr@gmail.com>
2018-01-25 15:24:11 -0600

    Update protocol.h

--- src/protocol.h
+++ src/protocol.h
@@ -18,7 +18,7 @@
 extern bool fTestNet;
 static inline unsigned short GetDefaultPort(const bool testnet = fTestNet)
 {
-    return testnet ? 25724 : 32101;
+    return testnet ? 25724 : 32001;
 }
 
 
